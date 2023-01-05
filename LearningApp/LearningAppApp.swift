ef8e964f2d7b52b37e1d0350d86272098584af1c
Feenok <ernestmargariti@gmail.com>
2023-01-04 19:36:46 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Ernest Margariti on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
